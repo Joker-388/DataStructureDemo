e4ff07a6f0ccfd1c4b80ab8efc2ef6626c308eb8
Lucky <yanbing.hu@icloud.com>
2019-05-18 19:34:21 +0800

    利用Map实现Set

--- objective_c_custom/LinkedList/LinkedList/LinkedList/LinkedCircleList.h
+++ objective_c_custom/LinkedList/LinkedList/LinkedList/LinkedCircleList.h
@@ -14,49 +14,54 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LinkedCircleListNode : NSObject
 
+/// 节点保存的元素
 @property (nonatomic, strong, nullable) id element;
+/// 下一个节点，防止只有一个节点的时候出现循环引用
 @property (nonatomic, weak, nullable) LinkedCircleListNode *weakNext;
+/// 下一个节点
 @property (nonatomic, strong, nullable) LinkedCircleListNode *next;
+/// 上一个节点
 @property (nonatomic, weak, nullable) LinkedCircleListNode *prev;
 
 - (instancetype)init __unavailable;
 + (instancetype)new __unavailable;
+/// 初始化节点
 - (instancetype)initWithPrev:(LinkedCircleListNode *)prev element:(nullable id)element next:(nullable LinkedCircleListNode *)next;
 
 @end
 
 @interface LinkedCircleList<ObjectType> : NSObject
 
+/// 头节点
 @property (nonatomic, strong, nullable) LinkedCircleListNode *first;
+/// 尾节点
 @property (nonatomic, strong, nullable) LinkedCircleListNode *last;
 
+/// 添加一个元素
 - (void)addObject:(nullable ObjectType)object;
+/// 插入一个元素
 - (void)addObject:(nullable ObjectType)object addIndex:(NSUInteger)index;
-
+/// 删除index对应的元素
 - (ObjectType)removeObjectAtIndex:(NSInteger)index;
+/// 删除元素
 - (ObjectType)removeObject:(nullable ObjectType)object;
-
+/// 第一个元素
 - (ObjectType)firstObject;
+/// 删除最后一个元素
 - (void)removeLastObject;
-
+/// 元素个数
 - (NSUInteger)count;
+/// 获取元素的index
 - (NSInteger)indexOfObject:(nullable ObjectType)object;
+/// 是否包含元素
 - (BOOL)containsObject:(nullable ObjectType)object;
+/// 删除所有元素
 - (void)removeAllObjects;
-
+/// 获取对应index的元素
 - (ObjectType)objectAtIndex:(NSUInteger)index;
+/// 替换对应index的元素
 - (void)replaceObjectAtIndex:(NSUInteger)index withObject:(ObjectType)object;
 
-- (void)insertValue:(id)value inPropertyWithKey:(NSString *)key __unavailable;
-- (void)insertValue:(id)value atIndex:(NSUInteger)index inPropertyWithKey:(NSString *)key __unavailable;
-- (void)removeValueAtIndex:(NSUInteger)index fromPropertyWithKey:(NSString *)key __unavailable;
-- (void)addObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options context:(nullable void *)context __unavailable;
-- (void)removeObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath __unavailable;
-- (void)removeObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath context:(nullable void *)context __unavailable;
-- (id)replacementObjectForCoder:(NSCoder *)aCoder __unavailable;
-- (id)replacementObjectForKeyedArchiver:(NSKeyedArchiver *)archiver __unavailable;
-- (void)replaceValueAtIndex:(NSUInteger)index inPropertyWithKey:(NSString *)key withValue:(id)value __unavailable;
-- (BOOL)respondsToSelector:(SEL)aSelector __unavailable;
 
 @end
 
--- objective_c_custom/LinkedList/LinkedList/LinkedList/SingleCircleLinkedList.h
+++ objective_c_custom/LinkedList/LinkedList/LinkedList/SingleCircleLinkedList.h
@@ -14,12 +14,16 @@ static const NSInteger SINGLE_LINKED_LIST_ELEMETN_NOT_FOUND = -1;
 
 @interface SingleCircleLinkedListNode : NSObject
 
+/// 节点保存的元素
 @property (nonatomic, strong, nullable) id element;
+/// 下一个节点，防止只有一个节点的时候出现循环引用
 @property (nonatomic, weak, nullable) SingleCircleLinkedListNode *weakNext;
+/// 下一个节点
 @property (nonatomic, strong, nullable) SingleCircleLinkedListNode *next;
 
 - (instancetype)init __unavailable;
 + (instancetype)new __unavailable;
+/// 初始化节点
 - (instancetype)initWithElement:(nullable id)element next:(nullable SingleCircleLinkedListNode *)next;
 
 @end
@@ -27,35 +31,33 @@ static const NSInteger SINGLE_LINKED_LIST_ELEMETN_NOT_FOUND = -1;
 
 @interface SingleCircleLinkedList<ObjectType> : NSObject
 
+/// 头节点
 @property (nonatomic, strong, nullable) SingleCircleLinkedListNode *first;
 
+/// 添加一个元素
 - (void)addObject:(nullable ObjectType)object;
+/// 插入一个元素
 - (void)addObject:(nullable ObjectType)object addIndex:(NSUInteger)index;
-
+/// 删除index对应的元素
 - (ObjectType)removeObjectAtIndex:(NSUInteger)index;
+/// 删除元素
 - (ObjectType)removeObject:(nullable ObjectType)object;
-
+/// 第一个元素
 - (ObjectType)firstObject;
+/// 删除最后一个元素
 - (void)removeLastObject;
-
+/// 元素个数
 - (NSUInteger)count;
+/// 获取元素的index
 - (NSInteger)indexOfObject:(nullable ObjectType)object;
+/// 是否包含元素
 - (BOOL)containsObject:(nullable ObjectType)object;
+/// 删除所有元素
 - (void)removeAllObjects;
-
+/// 获取对应index的元素
 - (ObjectType)objectAtIndex:(NSUInteger)index;
+/// 替换对应index的元素
 - (void)replaceObjectAtIndex:(NSUInteger)index withObject:(ObjectType)object;
-
-- (void)insertValue:(id)value inPropertyWithKey:(NSString *)key __unavailable;
-- (void)insertValue:(id)value atIndex:(NSUInteger)index inPropertyWithKey:(NSString *)key __unavailable;
-- (void)removeValueAtIndex:(NSUInteger)index fromPropertyWithKey:(NSString *)key __unavailable;
-- (void)addObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options context:(nullable void *)context __unavailable;
-- (void)removeObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath __unavailable;
-- (void)removeObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath context:(nullable void *)context __unavailable;
-- (id)replacementObjectForCoder:(NSCoder *)aCoder __unavailable;
-- (id)replacementObjectForKeyedArchiver:(NSKeyedArchiver *)archiver __unavailable;
-- (void)replaceValueAtIndex:(NSUInteger)index inPropertyWithKey:(NSString *)key withValue:(id)value __unavailable;
-- (BOOL)respondsToSelector:(SEL)aSelector __unavailable;
 
 @end
 
